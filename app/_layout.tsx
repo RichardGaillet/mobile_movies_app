ecbff4d7596fb2031ef2eb53e3e007b418012533
RichardGaillet <gaillet.richard@gmail.com>
2025-03-12 18:04:50 +0100

    Hide statusbar.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,22 @@
+import { StatusBar } from "react-native"
 import { Stack } from "expo-router"
 import "./globals.css"
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="movies/[id]"
-        options={{ headerShown: false }}
-      />
-    </Stack>
+    <>
+      <StatusBar hidden={true} />
+
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="movies/[id]"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
   )
 }
